51e8c84caec785798270da8e70eea23f92d6c77a
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-04-13 16:28:50 +0530

    Added pades

---
+++ main.js
@@ -0,0 +1,5 @@
+function scrollToFooter() {
+    var footer = document.getElementById("footer"); // Get the footer element by its ID
+    footer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the footer element with smooth animation
+}
+  
