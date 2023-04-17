837e7960487ea5f13b2233b32f4a339618a28b53
Mel.b <68156268+melbinjacob@users.noreply.github.com>
2023-04-17 12:19:03 +0530

    Brands page responsive

--- main.js
+++
@@ -1,5 +0,0 @@
-function scrollToFooter() {
-    var footer = document.getElementById("footer"); // Get the footer element by its ID
-    footer.scrollIntoView({ behavior: 'smooth' }); // Scroll to the footer element with smooth animation
-}
-  
